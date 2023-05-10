71757b3e46514fe56aaca79ef51f31f4ff3db297
gon0821 <gonty821@gmail.com>
2023-05-10 16:36:28 +0900

    20230509

--- week5-6/database/practice.sql
+++ week5-6/database/practice.sql
@@ -1,24 +1,24 @@
-CREATE table classmate
-  (id                INTEGER,
-  name               VARCHAR(100) NOT NULL,
-  birthday DATE NOT NULL,
-  PRIMARY KEY (id));
-
-CREATE table members
-  (id INTEGER NOT NULL,
-  name VARCHAR(128) NOT NULL,
-  );
-
-SELECT
-  DISTINCT(dept_no) AS uniq_dept_no
-FROM
-  dept_manager;
-
 SELECT
   *
 FROM
-  employees
-ORDER BY
-  birth_date ASC,
-  emp_no DESC
-LIMIT 100;
+  departments AS d INNER JOIN current_dept_emp AS c
+    ON d.dept_no=c.dept_no
+WHERE
+  c.to_date='9999-01-01'
+LIMIT
+  10;
+
+SELECT
+  d.dept_no,
+  d.dept_name,
+  s.salary
+FROM
+  salaries AS s
+  INNER JOIN dept_manager AS m
+    ON s.emp_no=m.emp_no
+  INNER JOIN departments AS d
+    ON m.dept_no=d.dept_no
+WHERE
+  m.to_date='9999-01-01'
+  AND s.to_date='9999-01-01'
+;
