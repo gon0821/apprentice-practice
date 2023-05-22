94f0093017c388a311d2b83a21bdd63e6fbdfda1
gon0821 <121909372+gon0821@users.noreply.github.com>
2023-05-22 21:24:36 +0900

    Update create_table.sql

--- week5-6/internet_tv/database_sql/create_table.sql
+++ week5-6/internet_tv/database_sql/create_table.sql
@@ -12,7 +12,6 @@ CREATE TABLE programs (
 CREATE TABLE program_genre (
   program_id BIGINT ,
   genre_id   INT ,
-
   PRIMARY KEY (program_id, genre_id),
   FOREIGN KEY (program_id)    REFERENCES programs(program_id),
   FOREIGN KEY (genre_id)      REFERENCES genres(genre_id)
@@ -22,7 +21,6 @@ CREATE TABLE program_seasons (
   program_season_id BIGINT   PRIMARY KEY AUTO_INCREMENT,
   program_id        BIGINT   NOT NULL,
   season_num        INTEGER  NOT NULL,
-
   UNIQUE (program_id, season_num),
   FOREIGN KEY (program_id)      REFERENCES programs(program_id)
 );
@@ -35,7 +33,6 @@ CREATE TABLE episodes (
   duration           TIME          NOT NULL,
   release_date       DATE          NOT NULL,
   program_season_id  BIGINT        NOT NULL,
-
   UNIQUE (episode_num, program_season_id),
   FOREIGN KEY (program_season_id)      REFERENCES program_seasons(program_season_id)
 );
@@ -52,7 +49,6 @@ CREATE TABLE lists (
   episode_id    BIGINT     NOT NULL,
   channel_id    INT        NOT NULL,
   views         BIGINT     NOT NULL,
-  
   UNIQUE (start_time, channel_id),
   FOREIGN KEY (episode_id)      REFERENCES episodes(episode_id),
   FOREIGN KEY (channel_id)      REFERENCES channels(channel_id)
