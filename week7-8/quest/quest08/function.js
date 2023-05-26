5b32b8552d681ee6c4bf5bbe6b42687648cef75e
gon0821 <gonty821@gmail.com>
2023-05-27 00:51:11 +0900

    quest8まで

---
+++ week7-8/quest/quest08/function.js
@@ -0,0 +1,13 @@
+console.log('Hello World');
+
+function greet(name) {
+  console.log(`Hello, ${name}`);
+}
+
+greet('Naoki');
+
+const introduce = (first_name, age) => {
+  console.log(`私の名前は${first_name}と言い、年齢は${age}歳です！`);
+}
+
+introduce('Naoki', 26);
